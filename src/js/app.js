6757971f84cf051b71d50ba060da6b0a7cfb84e9
Ivan Baturkin <ivanmysterys@gmail.com>
2021-12-15 23:55:07 +0200

    feat: implement simple tasks selection

---
+++ src/js/app.js
@@ -0,0 +1,23 @@
+const modelsObject = require('./models/models-obj');
+const tasks = require('./models/tasks');
+
+const getData = async () => {
+    const tasksRepo = modelsObject.tasksRepo;
+    await tasksRepo.sync();
+    const data = await tasksRepo.findAll();
+    return data;
+}
+
+const getTasks = async () => {
+    const tasks = [];
+    const data = await getData();
+    for (const model of data) {
+        tasks.push(model.dataValues);
+    }
+    return tasks;
+}
+
+void async function printTasks() {
+    const tasks = await getTasks();
+    console.log(tasks);
+}();
