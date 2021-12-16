cf6796ff5039ae6f66bae64e6906003d51760820
Ivan Baturkin <ivanmysterys@gmail.com>
2021-12-16 08:08:44 +0200

    feat: implement simple server

---
+++ src/js/server.js
@@ -0,0 +1,12 @@
+const app = require('./app');
+const { modelsObj, sequelize } = require('./models/modelsObj');
+const dotenv = require('dotenv').config();
+
+(async () => {
+  modelsObj();
+  await sequelize.sync({ force: false });
+
+  app.listen(process.env.EXPRESS_PORT, () => {
+    console.log(`Server listening at port ${process.env.EXPRESS_PORT}`);
+  });
+})();
